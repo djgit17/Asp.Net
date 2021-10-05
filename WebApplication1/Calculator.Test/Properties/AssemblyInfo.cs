2940086233d358ad3d658df87cea1e519377a90e
Darryl <Darryl@gmail.com>
2021-10-05 16:56:54 +0530

    First commit

---
+++ WebApplication1/Calculator.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Calculator.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculator.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df0d5024-e296-4c27-8b0e-d3bd47daad85")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebApplication1/Calculatordll/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Calculatordll
+{
+    public class Calculator
+    {
+        public static int Divide(int num, int dino)
+        {
+            int result = num / dino;
+            return result;
+
+        }
+
+    }
+}
